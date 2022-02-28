a4ad5c2bc4b4879afb5a12a5db245559a1ceac5f
Ange <angesdc@gmail.com>
2022-02-28 16:04:50 +0100

    should work, python really is OP

--- README.md
+++ README.md
@@ -13,5 +13,11 @@ If not installed:
 
 example:
 	./harvest.sh "22499132d096a88cf6d6bcdf606295e28bcd6c0" "round1"
+		will output the top 20, and put the result in 'round1.csv'
 
 The contest handle is the last number in the URL of the contest. Should be 39 characters long.
+
+To add later :
+	- "map" that is used to transform pseudo into logins based on manually done configuration file
+	- on the same basis, adds in csv output a field corresponding to the number of points
+	- create a script that takes multiple .csv files and agregate them into one (maybe this already exist) in order to produce the final ranking (to do this i need the third "score" field in all rounds.csv outputs, and a configuration file that indicates a number of points for each place in the ranking)
--- harvest.py
+++ harvest.py
@@ -4,7 +4,8 @@ from sys import argv
 
 if  len(argv) != 3:
     print("Usage: ./harvest.sh \"Contest handle\" \"Output file name\"")
-    print("The contest handle is the last 39 digits of the contest URL")
+    print("The contest handle is the last 39 digits of the contest URL:")
+    print("Example: 21499132d096a88cf6d6bcdf606295e28bcd6c0")
     exit()
 client = codingame.Client()
 try:
@@ -12,21 +13,26 @@ try:
 except ValueError:
     print("Contest handle not well formatted, --h for help")
     exit()
+except codingame.exceptions.ClashOfCodeNotFound:
+    print("Contest not found. Please check the handle you entered (--h for help)")
+    exit()
 res = {};
 j = 0
 for i in coc.players:
-    if j > 20:
+    if j >= 20:
         break
-    res[i.pseudo] = i.rank
+    res[i.rank] = i.pseudo
     j += 1
-res = sorted(res.items(), key=lambda x: x[1])
+res = sorted(res.items())
 try:
     with open(argv[2] + ".csv", 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
         writer.writerows(res)
+        error = 0
 except PermissionError:
-    print("Cannot open \"{}.csv\"".format(argv[2]))
-    exit()
+    print("\nCannot open \"{}.csv\": the output cant be saved\n".format(argv[2]))
+    error = 1
 for i in res:
-    print(i[1], ":", i[0])
-print("You will find this round top 20 in \"{}.csv\"".format(argv[2]))
+    print(i[0], ":", i[1])
+if (error != 1):
+    print("You will find this round top 20 in \"{}.csv\"".format(argv[2]))
